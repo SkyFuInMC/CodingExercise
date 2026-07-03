de064c5b08703d4575fe1e4da254a694955e0e7f
SkyFuInMC <skyfuinmc@outlook.com>
2026-07-03 21:52:44 +0800

    Microsoft Learn: Store and iterate through sequences of data using Arrays and the foreach statement in C#

---
+++ csharp/ms13-arrays/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program {
+	public static void Main() {
+		string[] orderIDs = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" ];
+		foreach (string orderID in orderIDs) {
+			if (orderID.StartsWith("B")) {
+				Console.WriteLine(orderID);
+			};
+		}
+	}
+}
